5cb0ec53ccaec4168ed45b5fe582f48ffbc2490c
Youngju Kim <pydemia@gmail.com>
2017-04-19 12:26:05 +0900

    Add files via upload

---
+++ src/op/float_operators.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def plus(a, b): 
+    return float(a + b) 
+
+def minus(a, b): 
+    return float(a - b) 
+
+def div(a, b): 
+    return float(a / b)
+
+def mul(a, b):
+     return float(a * b) 
---
+++ src/op/int_operators.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def plus(a, b): 
+    return int(a + b) 
+
+def minus(a, b): 
+    return int(a - b) 
+
+def div(a, b): 
+    return int(a / b)
+
+def mul(a, b):
+     return int(a * b) 
