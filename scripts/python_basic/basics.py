77f51b087e40d9eb6760e1a06036681d6e2773c4
Youngju Kim <pydemia@gmail.com>
2017-06-05 01:00:22 +0900

    Update basics.py

--- scripts/python_basic/basics.py
+++ scripts/python_basic/basics.py
@@ -389,6 +389,53 @@ watch3.elapse()
 watch3.stop()
 
 
+#%% 특수 메소드
+
+class myobject(object):
+    
+    def __init__(self, obj, name):
+        
+        # Check if listobj is list type OR just you can change it to list
+        if not isinstance(obj, list):
+            obj = list(obj)
+        
+        self.data = obj
+        self.name = name
+        #self.InitData(data)
+    
+    def __new__(cls, *args, **kwargs):
+        #return super(myobject, cls).__new__(cls)
+        return super().__new__(cls)
+    
+    def __str__(self):
+        return str(self.data)
+    
+    #def __call__(self):
+    #    print(self.name)
+    #    print(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
+    def __setitem__(self, index, value):
+        self.data[index] = value
+        #return self.data
+    
+    #def __delitem__(self):
+    #    pass
+    
+
+aa = myobject([1,2,3], 'This is my own Object.')
+ab = myobject((1,2,3), 'This is my own Object.')
+
+aa.data
+
+type(aa)
+aa
+aa[1]
+aa[2] = 4
+
+
 #%% 모듈, 패키지
 
 from src.mod import mymodule as md
